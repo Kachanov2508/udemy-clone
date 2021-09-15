adb9f8c0068857bb489a55f7cc301ce143a56b93
kachanov2508 <kachanov2508@yandex.ru>
2021-09-15 10:14:22 +0300

    csrf

--- client/context/index.js
+++ client/context/index.js
@@ -1,4 +1,6 @@
 import { useReducer, createContext, useEffect } from "react";
+import axios from "axios";
+import router from "next/router";
 
 // initial state
 const initialState = {
@@ -30,6 +32,45 @@ const Provider = ({ children }) => {
 			payload: JSON.parse(window.localStorage.getItem("user")),
 		});
 	}, []);
+
+	axios.interceptors.response.use(
+		function (response) {
+			// any status code that lie within the range of 2XX cause this function
+			// to trigger
+			return response;
+		},
+		function (error) {
+			// any status codes that falls outside the range of 2XX cause this function
+			// to trigger
+			let res = error.response;
+			if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+				return new Promise((resolve, reject) => {
+					axios
+						.get("/api/logout")
+						.then((data) => {
+							console.log("/401 error > logout");
+							dispatch({ type: "LOGOUT" });
+							window.localStorage.removeItem("user");
+							router.push("/login");
+						})
+						.catch((err) => {
+							console.log("AXIOS INTERCEPTORS ERR", err);
+							reject(error);
+						});
+				});
+			}
+			return Promise.reject(error);
+		}
+	);
+
+	useEffect(() => {
+		const getCsrfToken = async () => {
+			const { data } = await axios.get("/api/csrf-token");
+			// console.log("CSRF", data);
+			axios.defaults.headers["X-CSRF-Token"] = data.getCsrfToken;
+		};
+		getCsrfToken();
+	}, [])
 
 	return <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>;
 };
